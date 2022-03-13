cbf368db6bb4c827c662fac534d24abdc80d425d
SaguaroMaster <krisi889@gmail.com>
2022-03-13 15:56:59 +0100

    c

--- src/pid_node/src/ros_PID_node.py
+++ src/pid_node/src/ros_PID_node.py
@@ -37,38 +37,24 @@ def callback1(data):
     global rpm_array1
     rpm_array1 = data.data
 
-    try:
-        target_speed_old
-    except NameError:
-        target_speed_old = 0
-
-
     avg_rpm_rear = (rpm_array1[2]+rpm_array1[3])/2
     speed = (avg_rpm_rear/60)*0.355; 
 
     pid.setpoint = target_speed
 
     if (target_speed != 0):
-        Kf = 180 / target_speed
+        Kf = 180
         if (pid.auto_mode != True):
             pid.auto_mode = True
+        pid_output = pid(speed) + (target_speed * abs(Kf))
+        throttle_target = pid_output+1455
     else: 
         Kf = 0
         pid.auto_mode = False
-
-    if (target_speed < 0 and target_speed_old > 0):
-        sender(1455)
-        sleep(0.2)
-    target_speed_old = target_speed
-
-    if (target_speed == 0):
         throttle_target = 1455
-    else:
-        pid_output = pid(speed) + (target_speed * 0)
-        throttle_target = pid_output+1455
 
-    if throttle_target >= 2500 :
-        throttle_target = 2500
+    if throttle_target >= 2000 :
+        throttle_target = 2000
     elif throttle_target <= 1000 :
         throttle_target = 1000
 
