64a2a7675a2dd0092a09fdf50cf1581045ec536b
SaguaroMaster <krisi889@gmail.com>
2022-03-14 16:11:37 +0100

    c

--- src/twist_to_steering_angle/src/twist_to_steering_angle.py
+++ src/twist_to_steering_angle/src/twist_to_steering_angle.py
@@ -33,7 +33,7 @@ def callback1(data):
         steer_us = 1455'''
     
     #steer_deg=angular_speed*57.3
-    steer_us = steer_rad * 923,68 + 1455
+    steer_us = steer_rad * 924 + 1455
 
     sender(steer_us, target_speed)
 
